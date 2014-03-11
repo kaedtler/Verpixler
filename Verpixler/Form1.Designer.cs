913d2627e3cc546cc89dc07b8a03c9d180fcada9
Oliver Kädtler <okmaster92@gmail.com>
2014-03-11 14:41:02 +0100

    Version 2 (b/w) und Version 3 (ARGB) hinzugefügt (schreiben)

--- Verpixler/Form1.Designer.cs
+++ Verpixler/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(58, 11);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,7 +46,8 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(119, 13);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(139, 11);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -52,11 +55,25 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(39, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(206, 46);
+            this.ClientSize = new System.Drawing.Size(226, 46);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -71,6 +88,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Verpixler/Form1.cs
+++ Verpixler/Form1.cs
@@ -15,64 +15,179 @@ namespace Verpixler
         public Form1()
         {
             InitializeComponent();
+
+            comboBox1.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            const byte version = 1;
+            byte version = Convert.ToByte(comboBox1.SelectedItem.ToString());
 
-            OpenFileDialog open = new OpenFileDialog();
-            open.Multiselect = false;
-            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK && File.Exists(open.FileName))
+            switch (version)
             {
-                FileInfo file = new FileInfo(open.FileName);
-
-                if (file.Length < int.MaxValue - 9)
-                {
-                    byte[] size = BitConverter.GetBytes(file.Length);
-
-                    int width = (int)Math.Ceiling(Math.Sqrt(Math.Ceiling(((double)file.Length + 9d) / 3)));
-                    Bitmap bmp = new Bitmap(width, width);
-
-                    int counter = 0;
-
-                    bmp.SetPixel(counter % width, counter / width, Color.FromArgb(version, size[0], size[1])); counter++;
-                    bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[2], size[3], size[4])); counter++;
-                    bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[5], size[6], size[7])); counter++;
-
-                    byte? r = null, g = null, b = null;
-
-                    FileStream stream = file.OpenRead();
-                    for (int byt = stream.ReadByte(); byt >= 0; byt = stream.ReadByte())
+                case 1:
                     {
-                        if (r == null)
-                            r = (byte)byt;
-                        else if (g == null)
-                            g = (byte)byt;
-                        else if (b == null)
+                        OpenFileDialog open = new OpenFileDialog();
+                        open.Multiselect = false;
+                        if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK && File.Exists(open.FileName))
                         {
-                            b = (byte)byt;
-                            bmp.SetPixel(counter % width, counter / width, Color.FromArgb((byte)r, (byte)g, (byte)b));
-                            r = g = b = null;
-                            counter++;
+                            FileInfo file = new FileInfo(open.FileName);
+
+                            if (file.Length < int.MaxValue - 9)
+                            {
+                                byte[] size = BitConverter.GetBytes(file.Length);
+
+                                int width = (int)Math.Ceiling(Math.Sqrt(Math.Ceiling(((double)file.Length + 9d) / 3)));
+                                Bitmap bmp = new Bitmap(width, width);
+
+                                int counter = 0;
+
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(version, size[0], size[1])); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[2], size[3], size[4])); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[5], size[6], size[7])); counter++;
+
+                                byte? r = null, g = null, b = null;
+
+                                using (FileStream stream = file.OpenRead())
+                                    for (int byt = stream.ReadByte(); byt >= 0; byt = stream.ReadByte())
+                                    {
+                                        if (r == null)
+                                            r = (byte)byt;
+                                        else if (g == null)
+                                            g = (byte)byt;
+                                        else if (b == null)
+                                        {
+                                            b = (byte)byt;
+                                            bmp.SetPixel(counter % width, counter / width, Color.FromArgb((byte)r, (byte)g, (byte)b));
+                                            r = g = b = null;
+                                            counter++;
+                                        }
+                                    }
+
+                                if (r != null)
+                                {
+                                    bmp.SetPixel(counter % width, counter / width, Color.FromArgb((byte)r, g == null ? 0 : (byte)g, b == null ? 0 : (byte)b));
+                                }
+
+                                SaveFileDialog save = new SaveFileDialog();
+                                save.Filter = "*.png|*.png";
+                                if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                                {
+                                    bmp.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                                    MessageBox.Show("Verpixeln erfolgreich beendet!");
+                                }
+                            }
+                            else
+                                MessageBox.Show("Datei ist zu groß!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                         }
                     }
-                    stream.Close();
-
-                    if (r != null)
+                    break;
+                case 2:
                     {
-                        bmp.SetPixel(counter % width, counter / width, Color.FromArgb((byte)r, g == null ? 0 : (byte)g, b == null ? 0 : (byte)b));
-                    }
+                        OpenFileDialog open = new OpenFileDialog();
+                        open.Multiselect = false;
+                        if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK && File.Exists(open.FileName))
+                        {
+                            FileInfo file = new FileInfo(open.FileName);
 
-                    SaveFileDialog save = new SaveFileDialog();
-                    save.Filter = "*.png|*.png";
-                    if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-                    {
-                        bmp.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                            if (file.Length < int.MaxValue - 9)
+                            {
+                                byte[] size = BitConverter.GetBytes(file.Length);
+
+                                int width = (int)Math.Ceiling(Math.Sqrt(Math.Ceiling(((double)file.Length + 9d))));
+                                Bitmap bmp = new Bitmap(width, width);
+
+                                int counter = 0;
+
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(version, version, version)); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[0], size[0], size[0])); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[1], size[1], size[1])); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[2], size[2], size[2])); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[3], size[3], size[3])); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[4], size[4], size[4])); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[5], size[5], size[5])); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[6], size[6], size[6])); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[7], size[7], size[7])); counter++;
+
+                                using (FileStream stream = file.OpenRead())
+                                    for (int byt = stream.ReadByte(); byt >= 0; byt = stream.ReadByte())
+                                    {
+                                        bmp.SetPixel(counter % width, counter / width, Color.FromArgb((byte)byt, (byte)byt, (byte)byt));
+                                        counter++;
+                                    }
+
+                                SaveFileDialog save = new SaveFileDialog();
+                                save.Filter = "*.png|*.png";
+                                if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                                {
+                                    bmp.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                                    MessageBox.Show("Verpixeln erfolgreich beendet!");
+                                }
+                            }
+                            else
+                                MessageBox.Show("Datei ist zu groß!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
                     }
-                }
-                else
-                    MessageBox.Show("Datei ist zu groß!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case 3:
+                    {
+                        OpenFileDialog open = new OpenFileDialog();
+                        open.Multiselect = false;
+                        if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK && File.Exists(open.FileName))
+                        {
+                            FileInfo file = new FileInfo(open.FileName);
+
+                            if (file.Length < int.MaxValue - 9)
+                            {
+                                byte[] size = BitConverter.GetBytes(file.Length);
+
+                                int width = (int)Math.Ceiling(Math.Sqrt(Math.Ceiling(((double)file.Length + 9d) / 4)));
+                                Bitmap bmp = new Bitmap(width, width);
+
+                                int counter = 0;
+
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(version, size[0], size[1], size[2])); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[3], size[4], size[5], size[6])); counter++;
+                                bmp.SetPixel(counter % width, counter / width, Color.FromArgb(size[7], 0, 0, 0)); counter++;
+
+                                byte? a = null, r = null, g = null, b = null;
+
+                                using (FileStream stream = file.OpenRead())
+                                    for (int byt = stream.ReadByte(); byt >= 0; byt = stream.ReadByte())
+                                    {
+                                        if (a == null)
+                                            a = (byte)byt;
+                                        else if (r == null)
+                                            r = (byte)byt;
+                                        else if (g == null)
+                                            g = (byte)byt;
+                                        else if (b == null)
+                                        {
+                                            b = (byte)byt;
+                                            bmp.SetPixel(counter % width, counter / width, Color.FromArgb((byte)a, (byte)r, (byte)g, (byte)b));
+                                            a = r = g = b = null;
+                                            counter++;
+                                        }
+                                    }
+
+                                if (a != null)
+                                {
+                                    bmp.SetPixel(counter % width, counter / width, Color.FromArgb((byte)a, r == null ? 0 : (byte)r, g == null ? 0 : (byte)g, b == null ? 0 : (byte)b));
+                                }
+
+                                SaveFileDialog save = new SaveFileDialog();
+                                save.Filter = "*.png|*.png";
+                                if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                                {
+                                    bmp.Save(save.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                                    MessageBox.Show("Verpixeln erfolgreich beendet!");
+                                }
+                            }
+                            else
+                                MessageBox.Show("Datei ist zu groß!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
+                    }
+                    break;
             }
         }
 
@@ -89,7 +204,6 @@ namespace Verpixler
                     byte version = bmp.GetPixel(0, 0).R;
                     switch (version)
                     {
-                        default:
                         case 1:
                             {
                                 int counter = 0;
@@ -141,6 +255,9 @@ namespace Verpixler
                                 else
                                     MessageBox.Show("Bild ist Fehlerhaft!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                             }
+                            break;
+                        default:
+                            MessageBox.Show("Format wird nicht unterstützt!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                             break;
                     }
                 }
